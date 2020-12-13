430d5127387b794014fc8c4a03f3911b152f59c5
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-12 20:33:41 -0800

    Render Rate it stars dynamically highlight colors on hover

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 function MovieDetails({ movie }) {
 
+  const [ highlighted, setHightlighted ] = useState(-1);
+
+  const highlightRate = (high) =>  {
+    setHightlighted(high);
+  }
+
   return (
-    <div>
+    <>
       { movie ? (
         <div>
-        <h1>{movie.title}</h1>
-        <p>{movie.description}</p>
-        <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 0 ? 'orange': ''} />
-        <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 1 ? 'orange': ''} />
-        <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 2 ? 'orange': ''} />
-        <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 3 ? 'orange': ''} />
-        <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 4 ? 'orange': ''} />
-        ({movie.no_of_ratings})
+          <h1>{movie.title}</h1>
+          <p>{movie.description}</p>
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 0 ? 'orange' : ''} />
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 1 ? 'orange' : ''} />
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 2 ? 'orange' : ''} />
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 3 ? 'orange' : ''} />
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 4 ? 'orange' : ''} />
+          ({movie.no_of_ratings})
+          <div className="rate-container">
+            <h2>Rate it</h2>
+            { [...Array(5)].map((element, idx) => {
+              return <FontAwesomeIcon
+                        key={idx}
+                        icon={faStar}
+                        className={highlighted > idx - 1 ? 'purple' : ''}
+                        onMouseEnter={() => highlightRate(idx)}
+                        onMouseLeave={() => highlightRate(-1)}
+                      />
+            })}
+          </div>
         </div>
       ): null }
-    </div>
+    </>
   );
 }
 
