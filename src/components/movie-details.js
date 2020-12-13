1d7e68aab5f314102033420ea3fead378950fce4
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-12 20:10:38 -0800

    Add five start dynamic ratings to fill orange color

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -10,7 +10,12 @@ function MovieDetails(props) {
         <div>
         <h1>{props.movie.title}</h1>
         <p>{props.movie.description}</p>
-        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 0 ? 'orange': ''} />
+        <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 1 ? 'orange': ''} />
+        <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 2 ? 'orange': ''} />
+        <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 3 ? 'orange': ''} />
+        <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 4 ? 'orange': ''} />
+        ({props.movie.no_of_ratings})
         </div>
       ): null }
     </div>
