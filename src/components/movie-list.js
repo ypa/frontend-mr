27881ee04687414dae4f037e6709813c24e7cefe
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-13 20:24:32 -0800

    Add some layout for edit and delete icons

--- src/components/movie-list.js
+++ src/components/movie-list.js
@@ -15,7 +15,7 @@ function MovieList(props) {
     <div>
       { props.movies && props.movies.map( movie => {
         return (
-          <div key={movie.id}>
+          <div key={movie.id} className="movie-item">
             <h2 onClick={() => movieClicked(movie)}>{movie.title}</h2>
             <FontAwesomeIcon icon={faEdit} onClick={() => editClicked(movie) } />
             <FontAwesomeIcon icon={faTrash} />
