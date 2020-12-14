0fbfe06cb27db1dca98d637ae32d2468ebde8ad6
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-13 20:44:18 -0800

    Add movie edit form

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
       <div className="layout">
         <MovieList movies={movies} movieClicked={loadMovie} editClicked={editClicked} />
         <MovieDetails movie={selectedMovie} updateMovie={loadMovie} />
-        <MovieForm movie={editedMovie} />
+        { editedMovie ? <MovieForm movie={editedMovie} /> : null }
       </div>
     </div>
   );
--- src/components/movie-form.js
+++ src/components/movie-form.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function MovieForm({ movie }) {
+
+  const [title, setTitle ] = useState(movie.title);
+  const [description, setDescription ] = useState(movie.description);
+
+  const updateClicked = () => {
+    console.log('updated');
+  }
+
   return (
     <>
       { movie ? (
-        <h1>{movie && movie.title} edit</h1>
+        <div>
+          <label htmlFor="title">Title</label><br/>
+          <input id="title" type="text" placeholder="title" value={title}
+            onChange={event => setTitle(event.target.value)}
+          /><br/>
+          <label htmlFor="description">Description</label><br/>
+          <textarea id="description" type="text" placeholder="description" value={description}
+            onChange={event => setDescription(event.target.value)}
+          ></textarea><br/>
+          <button onClick={updateClicked}>Update</button>
+        </div>
       ) : null }
     </>
   );
