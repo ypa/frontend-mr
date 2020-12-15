1a4e103484065b598bfba7351446f04060c2b8dc
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-14 21:12:01 -0800

    Destructure props to simplify the MovieList component

--- src/components/movie-list.js
+++ src/components/movie-list.js
@@ -3,29 +3,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { API } from '../api-service';
 
-function MovieList(props) {
-  const movieClicked = (movie) => {
-    props.movieClicked(movie);
-  }
+function MovieList({ movies, movieClicked, editClicked, removeClicked }) {
 
-  const editClicked = (movie) => {
-    props.editClicked(movie);
-  }
-
-  const removeClicked = (movie) => {
+  const handleRemoveClicked = (movie) => {
     API.deleteMovie(movie.id)
-    .then(() => props.removeClicked(movie))
+    .then(() => removeClicked(movie))
     .catch(error => console.log(error));
   }
 
   return (
     <div>
-      { props.movies && props.movies.map( movie => {
+      { movies && movies.map( movie => {
         return (
           <div key={movie.id} className="movie-item">
             <h2 onClick={() => movieClicked(movie)}>{movie.title}</h2>
             <FontAwesomeIcon icon={faEdit} onClick={() => editClicked(movie) } />
-            <FontAwesomeIcon icon={faTrash} onClick={() => removeClicked(movie) } />
+            <FontAwesomeIcon icon={faTrash} onClick={() => handleRemoveClicked(movie) } />
           </div>
         )
       })}
